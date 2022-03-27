6f3aa8b2491ad0837d99924385016baef56ae128
Kooostenka <kostyakw@mail.ru>
2022-03-27 12:52:48 +0500

    release v1

---
+++ README.md
@@ -0,0 +1,1 @@
+Flexbox; js slider; page adaptation;
---
+++ index.php
@@ -0,0 +1,395 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Описание страницы">
+    <meta name="keywords" content="создание сайтов, верстка сайтов">
+
+
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="img/favicons/site.webmanifest">
+    <link rel="mask-icon" href="img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+
+
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cardo:ital,wght@0,700;1,400&family=Open+Sans:wght@700&family=Raleway:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <title>ActiveBox</title>
+</head>
+<body>
+
+<svg style="display:none">
+
+    <symbol id="facebook"  viewBox="0 0 64 64" >
+        <g>
+        <path d="M32,6C17 .642,6,6,17.642,6,32c0,13.035,9.603,23.799,22.113,25.679V38.89H21.68v-6.834h6.433v-4.548	c0-7.529,3.668-10.833,9.926-10.833c2.996,0,4.583,0.223,5.332,0.323v5.965h-4.268c-2.656,0-3.584,2.52-3.584,5.358v3.735h7.785	l-1.055,6.834h-6.73v18.843C48.209,56.013,58,45.163,58,32C58,17.642,46.359,6,32,6z"/>
+        </g>
+    </symbol>
+
+</svg>
+
+    <header class="header" id="header">
+        <div class="container">
+            <div class="header__inner">
+                <div class="header__logo">
+                    <span>ACTIVE</span>
+                </div>
+                <nav class="nav" id="nav">
+                    <a class="nav__link" href="#" data-scroll="#features">Features</a>
+                    <a class="nav__link" href="#" data-scroll="#works">Works</a>
+                    <a class="nav__link" href="#" data-scroll="#team">Our Team</a>
+                    <a class="nav__link" href="#" data-scroll="#reviews">Testimonials</a>
+                    <a class="nav__link" href="#" data-scroll="#download">Download</a>
+                </nav>
+
+                <button class="burger" type="button" id="navToggle">
+                    <span class="burger__item">Menu</span>
+                </button>
+            </div>
+        </div>
+
+    </header>
+
+    <div class="intro" id="intro">
+
+        <div class="container">
+
+            <div class="intro__inner">
+
+                <h1 class="intro__title">Your Favorite One Page Multi Purpose Template</h1>
+                <h2 class="intro__subtitle">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</h2>
+                <a class="btn btn--red" href="#">Find Out More</a>
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <div class="container" id="features">
+        <div class="features">
+            <div class="features__item">
+                <img src="img/imac.png" class="features__icon" alt="">
+                <h4 class="features__title">Первый блок</h4>
+                <div class="features__text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iure?
+                </div>
+            </div>
+
+            <div class="features__item">
+                <img src="img/imac.png" class="features__icon"/>
+                <h4 class="features__title">Первый блок</h4>
+                <div class="features__text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iure?
+                </div>
+            </div>
+
+            <div class="features__item">
+                <img src="img/imac.png" class="features__icon"/>
+                <h4 class="features__title">Первый блок</h4>
+                <div class="features__text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iure?
+                </div>
+            </div>
+
+            <div class="features__item">
+                <img src="img/imac.png" class="features__icon"/>
+                <h4 class="features__title">Первый блок</h4>
+                <div class="features__text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iure?
+                </div>
+            </div>
+
+            <div class="features__item">
+                <img src="img/imac.png" class="features__icon"/>
+                <h4 class="features__title">Первый блок</h4>
+                <div class="features__text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iure?
+                </div>
+            </div>
+
+            <div class="features__item">
+                <img src="img/imac.png" class="features__icon"/>
+                <h4 class="features__title">Первый блок</h4>
+                <div class="features__text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iure?
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="works" id="works">
+        <div class="works__item">
+            <img class="works__photo" src="img/lar.jpg" alt="">
+            <div class="works__content">
+                <div class="works__title">Project Name</div>
+                <div class="works__text">User Interface Design</div>
+            </div>
+        </div>
+
+        <div class="works__item">
+            <img class="works__photo" src="img/lar.jpg" alt="">
+            <div class="works__content">
+                <div class="works__title">Project Name</div>
+                <div class="works__text">User Interface Design</div>
+            </div>
+        </div>
+
+        <div class="works__item">
+            <img class="works__photo" src="img/lar.jpg" alt="">
+            <div class="works__content">
+                <div class="works__title">Project Name</div>
+                <div class="works__text">User Interface Design</div>
+            </div>
+        </div>
+
+        <div class="works__item">
+            <img class="works__photo" src="img/lar.jpg" alt="">
+            <div class="works__content">
+                <div class="works__title">Project Name</div>
+                <div class="works__text">User Interface Design</div>
+            </div>
+        </div>
+
+        <div class="works__item">
+            <img class="works__photo" src="img/lar.jpg" alt="">
+            <div class="works__content">
+                <div class="works__title">Project Name</div>
+                <div class="works__text">User Interface Design</div>
+            </div>
+        </div>
+
+        <div class="works__item">
+            <img class="works__photo" src="img/lar.jpg" alt="">
+            <div class="works__content">
+                <div class="works__title">Project Name</div>
+                <div class="works__text">User Interface Design</div>
+            </div>
+        </div>
+
+        <div class="works__item">
+            <img class="works__photo" src="img/lar.jpg" alt="">
+            <div class="works__content">
+                <div class="works__title">Project Name</div>
+                <div class="works__text">User Interface Design</div>
+            </div>
+        </div>
+
+        <div class="works__item">
+            <img class="works__photo" src="img/lar.jpg" alt="">
+            <div class="works__content">
+                <div class="works__title">Project Name</div>
+                <div class="works__text">User Interface Design</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="team" id="team">
+        <div class="container">
+            <div class="team__inner">
+
+                <div class="team__item">
+                    <img class="team__photo" src="img/sh2.jpeg" alt="">
+                    <div class="team__name">Ruth Woods</div>
+                    <div class="team__prof">Founder Ceo</div>
+                    <div class="team__text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quibusdam.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, nobis!</p>
+                    </div>
+                    <div class="social">
+
+                        <a href="#" class="social__item">
+                            <img src="img/facebook.png" width="18" alt="">
+                        </a>
+
+                    </div>
+
+                </div>
+
+                <div class="team__item">
+                    <img class="team__photo" src="img/sh2.jpeg" alt="">
+                    <div class="team__name">Ruth Woods</div>
+                    <div class="team__prof">Founder Ceo</div>
+                    <div class="team__text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quibusdam.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, nobis!</p>
+                    </div>
+                    <div class="social">
+
+                        <a href="#" class="social__item">
+                            <img src="img/facebook.png" width="18" alt="">
+                        </a>
+
+                    </div>
+
+                </div>
+
+
+                <div class="team__item">
+                    <img class="team__photo" src="img/sh2.jpeg" alt="">
+                    <div class="team__name">Ruth Woods</div>
+                    <div class="team__prof">Founder Ceo</div>
+                    <div class="team__text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quibusdam.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, nobis!</p>
+                    </div>
+                    <div class="social">
+
+                        <a href="#" class="social__item">
+                            <img src="img/facebook.png" width="18" alt="">
+                        </a>
+
+                    </div>
+
+                </div>
+
+                <div class="team__item">
+                    <img class="team__photo" src="img/sh2.jpeg" alt="">
+                    <div class="team__name">Ruth Woods</div>
+                    <div class="team__prof">Founder Ceo</div>
+                    <div class="team__text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quibusdam.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, nobis!</p>
+                    </div>
+                    <div class="social">
+
+                        <a href="#" class="social__item">
+                            <img src="img/facebook.png" width="18" alt="">
+                        </a>
+
+                    </div>
+
+                </div>
+
+
+            </div> <!-- /.inner -->
+
+        </div> <!-- /.container -->
+    </div><!-- /.team -->
+
+
+    <div class="reviews" id="reviews">
+        <div class="container">
+
+            <div class="reviews__slider" id="reviewsSlider">
+                <div class="slick-item">
+                    <div class="reviews__item">
+                        <div class="reviews__photo">
+                            <img class="reviews__img" src="img/car.jpg" alt="">
+                        </div>
+                        <div class="reviews__content">
+                            <div class="reviews__text">Lorem1 ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, dolore.</div>
+                            <div class="reviews__author">Lorem ipsum dolor sit amet.</div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="slick-item">
+                    <div class="reviews__item">
+                        <div class="reviews__photo">
+                            <img class="reviews__img" src="img/car.jpg" alt="">
+                        </div>
+                        <div class="reviews__content">
+                            <div class="reviews__text">Lorem2 ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, dolore.</div>
+                            <div class="reviews__author">Lorem ipsum dolor sit amet.</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div><!-- /#reviewsSlider -->
+
+        </div><!-- /.container -->
+    </div><!-- /.reviews -->
+
+    <div class="download" id="download">
+
+        <div class="container">
+            <h3 class="download__title">Are you ready?</h3>
+            <div class="download__text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, mollitia.</p>
+            </div>
+            <a href="#" class="btn btn--red btn--long">Download Here</a>
+        </div>
+
+    </div><!-- /.download -->
+
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__inner">
+
+                <div class="footer__block">
+                    <h4 class="footer__title">Location</h4>
+                    <div class="footer__text">
+                        <address class="footer__address">
+                            <p>3481 Melrose Place</p>
+                            <p>Beverly Hills, CA 90210</p>
+                        </address>
+                    </div>
+                </div>
+
+                <div class="footer__block">
+                    <h4 class="footer__title">Share with love</h4>
+                    <div class="footer__text">
+                        <div class="social social--footer">
+
+                            <a href="#" class="social__item">
+                                <img src="img/facebook.png" width="18" alt="">
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="footer__block">
+                    <h4 class="footer__title">About activebox</h4>
+                    <div class="footer__text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, ipsam.</p>
+                    </div>
+                </div>
+
+            </div><!-- /.footer-inner -->
+        </div><!-- /.container -->
+
+        <div class="copyright">
+
+            <div class="container">
+
+                <div class="copyright__text">
+                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, quis!</div>
+                    <div>Made with Y <span>by Kostya</span></div>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+<script src="app.js"></script>
+
+    
+</body>
+</html>
